12a509884529ed1a35383752c6d23b06d3f54257
Kapil Sharma <kapilsharma.8295@gmail.com>
2019-06-06 12:46:04 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,15 +118,16 @@ USE_L10N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'staticfiles'),
-#)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #----
 LOGIN_URL = 'login'
@@ -134,7 +135,7 @@ LOGOUT_URL = 'logout'
 LOGIN_REDIRECT_URL = 'main_home'
 
 # For backend feature
-#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 #EMAIL_HOST = 'smtp.gmail.com'
 #EMAIL_PORT = 587
