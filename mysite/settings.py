0a17c8e75624fb941f3694dae2988bf75e5a4b64
Kapil Sharma <kapilsharma.8295@gmail.com>
2019-06-06 13:15:44 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = (
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 #----
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
